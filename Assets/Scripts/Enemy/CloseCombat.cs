b32d5ed54e7118a082769a25e231955190a6b9d3
Brandon <brand.ld@1994@gmail.com>
2016-01-29 01:09:43 +0000

    Starting Enemy States

---
+++ Assets/Scripts/Enemy/CloseCombat.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CloseCombat : EnemyStates 
+{
+	public void Enter(Enemy enemy){}
+	public void Execute(){}
+	public void Exit(){}
+	public void OnTriggerEnter(Collider2D other){}
+
+}
---
+++ Assets/Scripts/Enemy/EnemyStates.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public interface EnemyStates
+{
+
+	void Enter(Enemy enemy);
+	void Execute();
+	void Exit();
+	void OnTriggerEnter(Collider2D other);
+
+
+
+}
